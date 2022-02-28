7c3016eb55201d4822f58399f9175d5780a31cbb
Tyler Bills <tbills1217@gmail.com>
2022-02-27 18:38:39 -0800

    commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function like(id) {
+    likes[id]++;
+    spans[id].innerHTML = likes[id] + "like(s)";
+}
+
+var likes = [9, 12, 9];
+var spans = [
+    document.querySelector("#post-1"),
+    document.querySelector("#post-2"),
+    document.querySelector("#post-3")
+];
